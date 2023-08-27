62abe1afbccc1542f1e11f40420182e74edae1cd
sefeen <faragsefen2@gmail.com>
2023-08-27 11:57:48 +0300

    handle

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,27 @@
+import EventKit
 import UIKit
 import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        requestCalendarAccess()
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    func requestCalendarAccess() {
+        let eventStore = EKEventStore()
+        eventStore.requestAccess(to: .event) { (granted, error) in
+            if granted {
+                print("Calendar access granted")
+            } else {
+                print("Calendar access denied or error occurred: \(error?.localizedDescription ?? "Unknown Error")")
+            }
+        }
+    }
 }
