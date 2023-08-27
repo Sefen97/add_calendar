215dd3eac983981e5aa64b917a3a22380233a996
sefeen <faragsefen2@gmail.com>
2023-08-27 08:37:09 +0300

    handle design

--- lib/main.dart
+++ lib/main.dart
@@ -11,55 +11,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Add event to calendar '),
-        ),
-        body: ListView(
-          children: [
-            ListTile(
-              title: const Text('Add normal event'),
-              trailing: const Icon(Icons.calendar_today),
-              onTap: () async {
-                final status = await Permission.calendar.request();
-                if (status.isGranted) {
-                  final event = Event(
-                    title: 'This is  title ',
-                    description: 'This is description ',
-                    location: 'This is location',
-                    startDate: DateTime.now().add(const Duration(minutes: 30)),
-                    endDate: DateTime.now().add(const Duration(minutes: 90)),
-                    allDay: false,
-                  );
-                  final success = await Add2Calendar.addEvent2Cal(event);
-                  if (success) {
-                    _dialogMessage(
-                        context: context,
-                        textMessage: "Event added to calendar successfully!",
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        });
-                  } else {
-                    _dialogMessage(
-                        context: context,
-                        textMessage: "Failed to add event to calendar.",
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        });
-                  }
-                } else {
-                  _dialogMessage(
-                      context: context,
-                      textMessage: "Calendar permission denied.",
-                      onTap: () {
-                        openAppSettings();
-                      });
-                }
-              },
+          appBar: AppBar(
+            title: const Text('Add event to calendar '),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: MaterialButton(
+                            color: Colors.blue,
+                            onPressed: () => _addEvent(context),
+                            child: const Text(
+                              "Add Event",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
-      ),
+          )),
     );
+  }
+
+  void _addEvent(BuildContext context) async {
+    final status = await Permission.calendar.request();
+    if (status.isGranted) {
+      final event = Event(
+        title: 'This is  title ',
+        description: 'This is description ',
+        location: 'This is location',
+        startDate: DateTime.now().add(const Duration(minutes: 30)),
+        endDate: DateTime.now().add(const Duration(minutes: 90)),
+        allDay: false,
+      );
+      final success = await Add2Calendar.addEvent2Cal(event);
+      if (success) {
+        _dialogMessage(
+            context: context,
+            textMessage: "Event added to calendar successfully!",
+            onTap: () {
+              Navigator.of(context).pop();
+            });
+      } else {
+        _dialogMessage(
+            context: context,
+            textMessage: "Failed to add event to calendar.",
+            onTap: () {
+              Navigator.of(context).pop();
+            });
+      }
+    } else {
+      _dialogMessage(
+          context: context,
+          textMessage: "Calendar permission denied.",
+          onTap: () {
+            openAppSettings();
+          });
+    }
   }
 
   Future<void> _dialogMessage(
